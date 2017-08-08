d55c17ba8c26859c6fc9c926d1f5185df4b7f86f
Tina La <tina.la47@gmail.com>
2017-08-08 13:45:53 -0400

    added container table view

---
+++ Eventcal/Eventcal/Views/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  Eventcal
+//
+//  Created by Tina La on 8/1/17.
+//  Copyright © 2017 Tina La. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+}
