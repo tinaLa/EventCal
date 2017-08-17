41706cd2b9e268af970f02db52e3b0f1f7eaec75
Tina La <tina.la47@gmail.com>
2017-08-17 13:21:27 -0400

    adjusted google places autocomplete view controller to prioritize locations closer to the user

--- Eventcal/Eventcal/Controllers/CreateEventViewController.swift
+++ Eventcal/Eventcal/Controllers/CreateEventViewController.swift
@@ -16,11 +16,14 @@ class CreateEventViewController: UIViewController {
     @IBOutlet weak var locationTextField: UITextField!
     
     let datePicker = UIDatePicker()
+    var placesClient: GMSPlacesClient!
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         createDatePicker()
         displayDateAndTime()
+        placesClient = GMSPlacesClient.shared()
     }
     
     // MARK: - Event Date and Time
@@ -54,10 +57,40 @@ class CreateEventViewController: UIViewController {
     
     // MARK: - Event Location
     @IBAction func locationTextFieldTapped(_ sender: Any) {
+        locationManager.requestWhenInUseAuthorization()
+        
         let autocompleteController = GMSAutocompleteViewController()
         autocompleteController.delegate = self
+        
+        getCurrentLocation { (coordinates) in
+            guard let currentLatitude = coordinates?.latitude else { return }
+            guard let currentLongitude = coordinates?.longitude else { return }
+            
+            let northeastBounds = CLLocationCoordinate2D(latitude: currentLatitude + 5.0, longitude: currentLongitude + 5.0)
+            let southwestBounds = CLLocationCoordinate2D(latitude: currentLatitude - 5.0, longitude: currentLongitude - 5.0)
+            
+            autocompleteController.autocompleteBounds = GMSCoordinateBounds(coordinate: northeastBounds, coordinate: southwestBounds)
+        }
+        
         present(autocompleteController, animated: true, completion: nil)
     }
+    
+    func getCurrentLocation(completion: @escaping (CLLocationCoordinate2D?) -> Void) {
+        placesClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    completion(place.coordinate)
+                }
+            }
+        })
+    }
+    
 }
 
 extension CreateEventViewController: GMSAutocompleteViewControllerDelegate {
